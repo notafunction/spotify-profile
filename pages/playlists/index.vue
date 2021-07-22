1b590c96656dfd231ac6abc22037c9b3b0ba0607
VPS <vps@mattlatham.dev>
2021-07-22 14:03:00 -0400

    filters out empty playlists

--- pages/playlists/index.vue
+++ pages/playlists/index.vue
@@ -2,7 +2,7 @@
   <div>
     <ListContainer title="Your Playlists" grid>
       <PlaylistItem
-        v-for="playlist in playlists"
+        v-for="playlist in playlistsWithTracks"
         :key="playlist.id"
         :playlist="playlist"
         large
@@ -24,6 +24,14 @@ export default {
       `${this.$config.spotifyApiUrl}/me/playlists`
     )
     this.playlists = items
+  },
+
+  computed: {
+    playlistsWithTracks() {
+      if (!this.playlists.length) return []
+
+      return this.playlists.filter((playlist) => playlist.tracks.total > 0)
+    },
   },
 }
 </script>
