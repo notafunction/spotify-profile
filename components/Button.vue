15c100acaf5e835214cdd6c857fbe6374336ffff
VPS <vps@mattlatham.dev>
2021-07-22 14:01:48 -0400

    renders link it to prop is given

--- components/Button.vue
+++ components/Button.vue
@@ -1,12 +1,22 @@
 <template>
-  <button
+  <component
+    :is="as"
+    :to="$attrs.to || null"
     class="px-6 py-3 text-white rounded-full border-2 border-white"
     v-on="$listeners"
   >
     <slot />
-  </button>
+  </component>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    as() {
+      if (this.$attrs.to) return 'nuxt-link'
+
+      return 'button'
+    },
+  },
+}
 </script>
