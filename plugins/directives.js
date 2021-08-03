1620ffe92f8c2dbdac6a94493708ff88a673e08a
VPS <vps@mattlatham.dev>
2021-08-03 16:10:13 -0400

    fixes directive

--- plugins/directives.js
+++ plugins/directives.js
@@ -1,17 +1,24 @@
 import Vue from 'vue'
 
 Vue.directive('click-outside', {
-  bind() {
-    this.event = (event) => this.vm.$emit(this.expression, event)
-    this.el.addEventListener('click', this.stopProp)
-    document.body.addEventListener('click', this.event)
+  inserted: (el, binding, vnode) => {
+    // assign event to the element
+    el.clickOutsideEvent = function (event) {
+      // here we check if the click event is outside the element and it's children
+      if (!(el === event.target || el.contains(event.target))) {
+        // if clicked outside, call the provided method
+        vnode.context[binding.expression](event)
+      }
+    }
+    // register click and touch events
+    document.body.addEventListener('click', el.clickOutsideEvent)
+    document.body.addEventListener('touchstart', el.clickOutsideEvent)
   },
-
-  unbind() {
-    this.el.removeEventListener('click', this.stopProp)
-    document.body.removeEventListener('click', this.event)
+  unbind(el) {
+    // unregister click and touch events before the element is unmounted
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+    document.body.removeEventListener('touchstart', el.clickOutsideEvent)
   },
-
   stopProp(event) {
     event.stopPropagation()
   },
